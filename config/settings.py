0d226618b2cc59e87572825ca294fa6b9818be63
Thomaz Botelho <thomazrb@gmail.com>
2024-06-08 18:01:26 -0300

    Pronto pro deploy

--- config/settings.py
+++ config/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['127.0.0.1','localhost','t1-base-django.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['https://t1-base-django.fly.dev/']
 # Application definition
 
 INSTALLED_APPS = [
